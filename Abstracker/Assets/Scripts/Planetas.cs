4afe0e1a730b46eab7df2ba1f6c52b1918b14031
Zuki <guillermitopereda@gmail.com>
2025-08-03 18:18:41 -0300

    mas planetas

--- Abstracker/Assets/Scripts/Planetas.cs
+++ Abstracker/Assets/Scripts/Planetas.cs
@@ -7,13 +7,13 @@ public class Planetas : MonoBehaviour
     public Score score;
     public AutoClicker autoClicker;
 
-    public Button polvoButton, lunaButton, planetaButton;
+    public Button polvoButton, meteoroButton, lunaButton, planetaButton, estrellaButton, agujeroNegroButton ;
     public TMP_Text multiplicadorTxt;
-    public TMP_Text polvoCostTxt, lunaCostTxt, planetaCostTxt;
+    public TMP_Text polvoCostTxt, meteoroCostTxt, lunaCostTxt, planetaCostTxt, estrellaCostTxt, agujeroNegroCostTxt;
 
-    int polvoCost = 10, lunaCost = 100, planetaCost = 1000;
+    int polvoCost = 10000, meteoroCost = 20000, lunaCost = 30000, planetaCost = 40000, estrellaCost = 50000, agujeroNegroCost = 60000;
 
-    public GameObject polvo, luna, planeta;
+    public GameObject polvo, meteoro, luna, planeta, estrella, agujeroNegro;
     public GameObject planetaUI;
     public GameObject planetas3D;
 
@@ -24,19 +24,26 @@ public class Planetas : MonoBehaviour
     void Start()
     {
         polvoButton.onClick.RemoveAllListeners();
+        meteoroButton.onClick.RemoveAllListeners();
         lunaButton.onClick.RemoveAllListeners();
         planetaButton.onClick.RemoveAllListeners();
+        estrellaButton.onClick.RemoveAllListeners();
+        agujeroNegroButton.onClick.RemoveAllListeners();
         PlanetasButton.onClick.RemoveAllListeners();
 
         polvoButton.onClick.AddListener(ComprarPolvo);
+        meteoroButton.onClick.AddListener((ComprarMeteoro));
         lunaButton.onClick.AddListener(ComprarLuna);
         planetaButton.onClick.AddListener(ComprarPlaneta);
+        estrellaButton.onClick.AddListener(ComprarEstrella);
+        agujeroNegroButton.onClick.AddListener(ComprarAgujeroNegro);
         PlanetasButton.onClick.AddListener(PlanetasButtonClicked);
 
-
-
+        meteoroButton.interactable = false;
         lunaButton.interactable = false;
         planetaButton.interactable = false;
+        estrellaButton.interactable = false;
+        agujeroNegroButton.interactable = false;
 
         autoClicker.SetMultiplier(1f); // Valor base
         UpdateMultiplicadorText();
@@ -44,8 +51,11 @@ public class Planetas : MonoBehaviour
         // Inicializar el texto del multiplicador
         multiplicadorTxt.text = "Multiplicador: x" + autoClicker.multiplier.ToString("0.0");
         polvoCostTxt.text = "Costo: " + polvoCost;
+        meteoroCostTxt.text = "Costo: " + meteoroCost;
         lunaCostTxt.text = "Costo: " + lunaCost;
         planetaCostTxt.text = "Costo: " + planetaCost;
+        estrellaCostTxt.text = "Costo: " + estrellaCost;
+        agujeroNegroCostTxt.text = "Costo: " + agujeroNegroCost;
     }
 
     void ComprarPolvo()
@@ -55,13 +65,27 @@ public class Planetas : MonoBehaviour
             score.AddPoints(-polvoCost);
             autoClicker.SetMultiplier(2f);
             polvoButton.interactable = false;
-            lunaButton.interactable = true;
+            meteoroButton.interactable = true;
             UpdateMultiplicadorText();
             polvo.SetActive(true);
             polvoCostTxt.enabled = false; // Desactiva el texto de costo de polvo
         }
     }
 
+    void ComprarMeteoro()
+    {
+        if (score.Points >= meteoroCost)
+        {
+            score.AddPoints(-meteoroCost);
+            autoClicker.SetMultiplier(3f);
+            meteoroButton.interactable = false;
+            lunaButton.interactable = true;
+            UpdateMultiplicadorText();
+            meteoro.SetActive(true);
+            polvo.SetActive(false);
+            meteoroCostTxt.enabled = false; // Desactiva el texto de costo de meteoro
+        }
+    }
     void ComprarLuna()
     {
         if (score.Points >= lunaCost)
@@ -71,7 +95,7 @@ public class Planetas : MonoBehaviour
             lunaButton.interactable = false;
             planetaButton.interactable = true;
             UpdateMultiplicadorText();
-            polvo.SetActive(false);
+            meteoro.SetActive(false);
             luna.SetActive(true);
             lunaCostTxt.enabled = false; // Desactiva el texto de costo de luna
         }
@@ -84,10 +108,40 @@ public class Planetas : MonoBehaviour
             score.AddPoints(-planetaCost);
             autoClicker.SetMultiplier(6f);
             planetaButton.interactable = false;
+            estrellaButton.interactable = true;
             UpdateMultiplicadorText();
             luna.SetActive(false);
             planeta.SetActive(true);
             planetaCostTxt.enabled = false; // Desactiva el texto de costo de planeta
+        }
+    }
+
+    void ComprarEstrella()
+    {
+        if (score.Points >= estrellaCost)
+        {
+            score.AddPoints(-estrellaCost);
+            autoClicker.SetMultiplier(8f);
+            estrellaButton.interactable = false;
+            agujeroNegroButton.interactable = true;
+            UpdateMultiplicadorText();
+            planeta.SetActive(false);
+            estrella.SetActive(true);
+            estrellaCostTxt.enabled = false; // Desactiva el texto de costo de estrella
+        }
+    }
+
+    void ComprarAgujeroNegro()
+    {
+        if (score.Points >= agujeroNegroCost)
+        {
+            score.AddPoints(-agujeroNegroCost);
+            autoClicker.SetMultiplier(10f);
+            agujeroNegroButton.interactable = false;
+            UpdateMultiplicadorText();
+            estrella.SetActive(false);
+            agujeroNegro.SetActive(true);
+            agujeroNegroCostTxt.enabled = false; // Desactiva el texto de costo de Agujero Negro
         }
     }
 
